128759dd28f03d9ef1778a2be4f51758e589220a
jonathanx111 <jonathanx111@gmail.com>
2021-01-12 18:24:01 -0500

    done with dark, currently on add to cart

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,17 @@
 import React from "react";
 
 function Item({ name, category }) {
+  // const []
+
+  function handleAddCartButton() {
+
+  }
+
   return (
     <li className="">
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={handleAddCartButton} className="add">Add to Cart</button>
     </li>
   );
 }
