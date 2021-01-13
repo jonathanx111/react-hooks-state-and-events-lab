bd62911727638b0e484bb1b4f1b22bbc635f0978
jonathanx111 <jonathanx111@gmail.com>
2021-01-12 22:53:56 -0500

    commit tree b442ba45b2570d598ce6d10d8861d71294d4294d

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,14 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
-  // const []
+  const [inCartOrOut, setInCartOrOut] = useState("")
 
   function handleAddCartButton() {
-
+    setInCartOrOut(inCartOrOut => inCartOrOut === "in-cart" ? inCartOrOut = "" : inCartOrOut = "in-cart")
   }
 
   return (
-    <li className="">
+    <li className={inCartOrOut}>
       <span>{name}</span>
       <span className="category">{category}</span>
       <button onClick={handleAddCartButton} className="add">Add to Cart</button>
